20fb5d0ce1c129dca76cdcdb85e0c5f73570d6b0
Vinokurov Alexander <aleksandr.vinokurov@infotecs.ru>
2018-01-09 23:37:50 +0300

    Комментарии к коду.

--- database.py
+++ database.py
@@ -1,8 +1,15 @@
 #! /usr/bin/env python
+# Зачем здесь это? Вы планируете использовать второй питон? Он через 2 года перестает поддерживаться
+# https://pythonclock.org/
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import create_engine, Column, Integer, String, Text, \
+# from sqlalchemy import create_engine, Column, Integer, String, Text, \
+#     DateTime, ForeignKey
+# Чище сделать вот так
+from sqlalchemy import (
+    create_engine, Column, Integer, String, Text,
     DateTime, ForeignKey
+)
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -12,26 +19,36 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 
-class Meteostation(Base):
-    __tablename__ = 'meteostations'
+# class Meteostation(Base):
+# Лучше использовать английские слова в названиях классов, мне PyCharm здесь показывает ошибку, т.к. не знает
+# слова Meteostation
+class WeatherStation(Base):
+    __tablename__ = 'weather_stations'
+    # Почему id строка?
     id = Column(String(), primary_key=True)
-    winds = relationship('Wind', backref='meteostation')
+    winds = relationship('Wind', backref='weather_station')
 
+    # Зачем это?
     def __init__(self, id=None):
         self.id = id
 
 
+# Не совсем понятное название класса
 class Wind(Base):
     __tablename__ = 'wind'
-    localdate = Column(DateTime, primary_key=True)
+    # localdate = Column(DateTime, primary_key=True)
+    # Опять PyCharm подсветил ошибку, т.к. не знает английского слова localdate
+    # Хорошо бы и для этой таблицы добавить поле id
+    local_date = Column(DateTime, primary_key=True)
     wind_speed = Column(Integer, nullable=False)
     wind_direction = Column(Text)
-    meteo_id = Column(
+    weather_station_id = Column(
         String(),
-        ForeignKey('meteostations.id'),
+        ForeignKey('weather_stations.id'),
         primary_key=True
     )
 
+    # Зачем это?
     def __init__(self,
                  localdate=None, wind_speed=None,
                  wind_direction=None, meteo_id=None):
@@ -42,4 +59,5 @@ class Wind(Base):
 
 
 if __name__ == "__main__":
+    # Явнее будет вынести это в какую-то отдельную функцию, например create_database
     Base.metadata.create_all(bind=engine)
