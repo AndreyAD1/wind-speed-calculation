739532040b034d65cb92ce2fa8049943e91fbe59
Gulnaz Maylunova <mailunova@gmail.com>
2018-01-27 17:30:11 +0300

    Fixes

--- server.py
+++ server.py
@@ -27,7 +27,7 @@ def calculate():
     return render_template(
         'calculate.html',
         velocity_table=velocity,
-        image=str(image_encoded)
+        image=str(image_encoded),
     )
 
 
