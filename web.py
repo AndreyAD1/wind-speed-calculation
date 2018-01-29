3bf65b066e1d104dfea9b5dcfb4343615af18f12
Gulnaz Maylunova <mailunova@gmail.com>
2018-01-29 15:57:22 +0300

    Renamed and added displaying tables in the web-interface

--- web.py
+++ web.py
@@ -21,14 +21,25 @@ def calculate():
     station_id = form['station_id']
     start_date = form['from']
     end_date = form['to']
+
     load_weather_data(station_id, start_date, end_date)
+    # TODO add time interval
     data = WindIndicator.query.filter(WindIndicator.weather_station_id==station_id).all()
-    velocity, _, image_buf = get_calculation_results(data)
+
+    filter(
+        WindIndicator.local_date
+
+        )
+
+
+    velocity, result_direction_speed, image_buf = get_calculation_results(data)
     image_encoded = base64.b64encode(image_buf.getvalue()).decode('utf-8')
+
     return render_template(
         'calculate.html',
         velocity_table=velocity,
         image=str(image_encoded),
+        result_direction_speed=result_direction_speed
     )
 
 
